a80e6e562e2a2dafb8663bd6ca1aa868c44bcf42
mrcbrbn5361 <brtapardi@gmail.com>
2025-01-23 13:05:41 +0300

    index.js ve reportWebVitals.js dosyaları yeniden oluşturuldu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
- 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/App.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Web performans metriklerini ölçmek için
+reportWebVitals(); 
